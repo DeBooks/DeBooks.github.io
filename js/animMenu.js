d97f67e084b861ae0ece67cc289b64fe06c1ec86
DeBooks <dbkreaders@gmail.com>
2017-05-10 21:10:42 -0300

    Probable optimización del script
    Ahora se ejecutará al momento de entrar en la página la animación
    "fadeOut". Además, las líneas en jQuery son básicamente 2 y agregué un
    $(function.....) para evitar que el script pueda ejecutarse antes de que
    la página termine de cargar.

--- js/animMenu.js
+++ js/animMenu.js
@@ -1,10 +1,14 @@
-function aniKEK() {
-	var lista = document.getElementById("lis");
-	if($("#lis").hasClass("fadeIn")){
-		$("#lis").removeClass("fadeIn");
-		$("#lis").addClass("fadeOut");
-	} else {
-		$("#lis").addClass("fadeIn");
-		$("#lis").removeClass("fadeOut");
-	}
-}
+$(function(){
+	function aniKEK() {
+		/*var lista = document.getElementById("lis");
+		if($("#lis").hasClass("fadeIn")){
+			$("#lis").removeClass("fadeIn");
+			$("#lis").addClass("fadeOut");
+		} else {
+			$("#lis").addClass("fadeIn");
+			$("#lis").removeClass("fadeOut");
+		}*/
+		$("#lis").toggleClass("fadeIn");
+		$("#lis").toggleClass("fadeOut");
+	}	
+});

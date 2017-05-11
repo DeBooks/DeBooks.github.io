6766c05ed8805a9487249f829b5d8abfb42fb5fa
DeBooks <dbkreaders@gmail.com>
2017-05-10 21:12:22 -0300

    Revert "Probable optimización del script"
    This reverts commit d97f67e084b861ae0ece67cc289b64fe06c1ec86.

--- js/animMenu.js
+++ js/animMenu.js
@@ -1,14 +1,10 @@
-$(function(){
-	function aniKEK() {
-		/*var lista = document.getElementById("lis");
-		if($("#lis").hasClass("fadeIn")){
-			$("#lis").removeClass("fadeIn");
-			$("#lis").addClass("fadeOut");
-		} else {
-			$("#lis").addClass("fadeIn");
-			$("#lis").removeClass("fadeOut");
-		}*/
-		$("#lis").toggleClass("fadeIn");
-		$("#lis").toggleClass("fadeOut");
-	}	
-});
+function aniKEK() {
+	var lista = document.getElementById("lis");
+	if($("#lis").hasClass("fadeIn")){
+		$("#lis").removeClass("fadeIn");
+		$("#lis").addClass("fadeOut");
+	} else {
+		$("#lis").addClass("fadeIn");
+		$("#lis").removeClass("fadeOut");
+	}
+}
